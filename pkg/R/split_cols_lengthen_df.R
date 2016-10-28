e0a5770b1c679461c91d4b718aaf59186e5eac99
Ivan Grishagin <Ivan Grishagin>
2016-10-28 15:25:42 -0400

    bugfix

--- pkg/R/split_cols_lengthen_df.R
+++ pkg/R/split_cols_lengthen_df.R
@@ -112,49 +112,7 @@ split_cols_lengthen_df <-
                                             ,at_once=TRUE)
             }
         }
-        
+        dFrame_proc[dFrame_proc=="NA"]<-
+            NA
         return(dFrame_proc)
     }
-
-        
-        
-        ############################## old version
-        # if(any(!colsToSplit %in% colnames(dFrame))){
-        #     stop("expand.df.via.split.col: Some colsToSplit values were not found among colnames!")
-        # }
-        # 
-        # for(colindex in 1:length(colsToSplit)){
-        #     #which rows are affected in the first column
-        #     rowsAffected<-
-        #         grep(patternToSplit
-        #              ,dFrame[,colsToSplit[colindex]])
-        #     
-        #     #if any rows are affected, proceed
-        #     if(length(rowsAffected)>0){
-        #         #split the affected rows in the first column by pattern
-        #         splitList<-
-        #             dFrame[rowsAffected,colsToSplit[colindex]] %>% 
-        #             strsplit(split=patternToSplit)
-        #         
-        #         #make a vector of how many replicates of each row to take
-        #         rowReplicates<-
-        #             rep(1,nrow(dFrame))
-        #         rowReplicates[rowsAffected]<-
-        #             sapply(splitList,length)
-        #         
-        #         finalRowsToTake<-
-        #             rep(1:nrow(dFrame)
-        #                 ,rowReplicates)
-        #         dFrame<-
-        #             dFrame[finalRowsToTake,]
-        #         rowsAffected_2<-
-        #             grep(patternToSplit
-        #                  ,dFrame[,colsToSplit[colindex]])
-        #         
-        #         #replace the values
-        #         dFrame[rowsAffected_2,colsToSplit[colindex]]<-
-        #             unlist(splitList)
-        #     }
-        # }
-        # 
-        # return(dFrame)
