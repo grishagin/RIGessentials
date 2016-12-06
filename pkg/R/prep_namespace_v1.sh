5e93d299d074e121d3c4cf6bd75b3437ac749aaa
Ivan Grishagin <Ivan Grishagin>
2016-12-06 15:20:27 -0500

    Version 1.9

---
+++ pkg/R/prep_namespace_v1.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+fun_names=(*.R)
+
+# add the first opening statement
+# overwrite the file 
+printf 'export(' > 'NAMESPACE'
+# append the first filename on the same line
+printf '"%s"\n' ${fun_names[@]:0:1} >> 'NAMESPACE'
+
+# loop through all functions' file names
+for f in ${fun_names[@]:1}
+	do 
+	# store output of a grep on each of those filenames
+	# to determine which of them do not belong (start witn "inner")
+	temp=$(printf '%s' "$f" | grep ^inner)
+	# if said output is longer than 0, ignore
+	if [ ${#temp} \< 1 ]
+	then
+		# else append to file
+		printf '\t\t,"%s"\n' ${f::-2} >> 'NAMESPACE'
+	fi
+done
+
+# append a closing bracket
+printf ')' >> 'NAMESPACE'
