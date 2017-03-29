5b5e79f9d7756536c0063ce275a582cbc6eb7add
Ivan Grishagin <Ivan Grishagin>
2017-03-29 14:45:46 -0400

    Version 2.8

--- pkg/R/prep_namespace_v4.sh
+++ pkg/R/prep_namespace_v4.sh
@@ -3,10 +3,10 @@ fun_names=(*.R)
 
 # add the first opening statement
 # overwrite the file 
-printf 'export(' > 'NAMESPACE'
+printf 'export(' > '../NAMESPACE'
 # append the first filename on the same line
 ff=${fun_names[@]:0:1}
-printf '"%s"\n' ${ff::-2} >> 'NAMESPACE'
+printf '"%s"\n' ${ff::-2} >> '../NAMESPACE'
 
 # loop through all functions' file names
 for f in ${fun_names[@]:1}
@@ -18,9 +18,9 @@ for f in ${fun_names[@]:1}
 	if [ ${#temp} \< 1 ]
 	then
 		# else append to file
-		printf '\t\t,"%s"\n' ${f::-2} >> 'NAMESPACE'
+		printf '\t\t,"%s"\n' ${f::-2} >> '../NAMESPACE'
 	fi
 done
 
 # append a closing bracket
-printf ')' >> 'NAMESPACE'
+printf ')' >> '../NAMESPACE'
