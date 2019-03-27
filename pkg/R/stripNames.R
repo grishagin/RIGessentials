851e458680f2118ae0fc50a61b985ad024f865fb
Ivan Grishagin <ivan.grishagin@ranchobiosciences.com>
2019-03-27 16:07:35 -0400

    v4.1.1: fixed merge_cols_shorten_df throwing invalid subscript type 'list' error. Removed legacy pkg folder.

--- pkg/R/stripNames.R
+++
@@ -1,8 +0,0 @@
-stripNames <-
-function(input){
-        tryCatch(names(input)<-NULL
-                 ,error=function(e){
-                     message("Can't strip the names. Please, check input.")
-                 })
-        return(input)
-    }
--- pkg/R/update_RIGessentials.R
+++
@@ -1,5 +0,0 @@
-update_RIGessentials<-
-    function(){
-        unloadNamespace(ns = "RIGessentials")
-        devtools::install_github("grishagin/RIGessentials",subdir = "pkg")
-    }
