2b05d2462720ea7c890b86fe6d5aeffb5a2f6dfc
Ivan Grishagin <ivan.grishagin@ranchobiosciences.com>
2019-03-27 22:38:58 -0400

    Added debugging code.

--- R/merge_cols_shorten_df.R
+++ R/merge_cols_shorten_df.R
@@ -45,7 +45,7 @@ merge_cols_shorten_df<-
     if(length(colsToMerge)>1){
       for (cndex in 2:length(colsToMerge)){
         dFrame_proc<-
-          dFrame[,.(newcol=paste(get(colsToMerge[cndex])
+          dFrame[,c(newcol=paste(get(colsToMerge[cndex])
                                  ,collapse=patternToMerge))
                  ,by=colKey]$newcol %>% 
           cbind(dFrame_proc
